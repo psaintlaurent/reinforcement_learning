5ca93c89da65d08c72227d2728806ab6c2a37db9
Patrick Saint - Laurent <patrick@saint-laurent.us>
2021-03-24 22:10:55 -0400

    More refactoring, cleanup of policy object and progress towards making the policy agent more generalizable for different learning methods

--- agents/policy.py
+++ agents/policy.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from numpy.random import default_rng
 
 """
 This needs major refactoring
@@ -6,18 +7,64 @@ The general idea is that this will eventually be a generic policy object
 that is managed by a Policy Iteration Agent
 """
 
-
 class Policy(object):
     observations = None
     returns = None
     returns_count = None
     optimal_state_action = None
+    time_steps = None
+    gamma = None  # discount rate
+    epsilon = None
 
     def __init__(self):
         self.observations = OrderedDict()
         self.returns = OrderedDict()
         self.returns_count = OrderedDict()
         self.optimal_state_action = OrderedDict()
+        self.time_steps = 20
+        self.gamma = .2
+        self.epsilon = .1
+
+    """
+           Initialize the policy by generating an episode following pi, 
+           an initially random epsilon soft policy.
+           The code for this is going to look odd.
+    """
+
+    def initialize(self, env):
+
+        """
+            Select the epsilon greedy action according to the policy.
+            If no observation was found choose the next action at random.
+        """
+        rng = default_rng(219215)
+        random_number = rng.random()
+
+        observation = env.reset()
+        current_state = observation
+        action = None
+
+        for idx in range(self.time_steps):
+
+            """ Select an action """
+            if random_number < self.epsilon:  # Explore
+                action = env.action_space.sample()
+            elif random_number >= self.epsilon:  # Exploit if there is an exploitable option
+                action, _ = self.next_action(current_state)
+                if action is None:
+                    action = env.action_space.sample()
+
+            """ Take the selected action """
+            observation, reward, done, _ = env.step(action)
+
+            """ Add a new state -> action mapping to the existing policy """
+            if current_state is not None:
+                self.add(current_state, reward, action)
+
+            """ Store the existing state, reward and action """
+            current_state, previous_reward, previous_action = observation, reward, action
+
+        return
 
     """
         This is a slightly better version but still inefficient method of keeping track of mappings of
@@ -29,22 +76,16 @@ class Policy(object):
         observation = tuple(observation)
         if observation not in self.observations:
             self.observations[observation] = [{
-                'next_action': next_action,
-                'next_reward': reward,
-                'discounted_returns': [],
+                'next_action': next_action, 'next_reward': reward, 'discounted_returns': [],
                 'average_returns': sum(self.observations[observation]) / len(self.observations[observation]),
-                'visits': None,
-                'probability': None
+                'visits': None, 'probability': None
             }]
 
         else:
             self.observations[observation].append({
-                'next_action': next_action,
-                'next_reward': reward,
-                'discounted_returns': [],
+                'next_action': next_action, 'next_reward': reward, 'discounted_returns': [],
                 'average_returns': sum(self.observations[observation]) / len(self.observations[observation]),
-                'visits': None,
-                'probability': None
+                'visits': None, 'probability': None
             })
 
         return
@@ -54,7 +95,8 @@ class Policy(object):
         observation = tuple(observation)
         if observation not in self.optimal_state_action or self.optimal_state_action[observation]['return'] <= \
                 self.returns[(observation, action)]:
-            self.optimal_state_action[observation] = {'action': action, 'return': self.returns[(observation, action)]}
+            self.optimal_state_action[observation] = {'action': action,
+                                                      'return': self.returns[(observation, action)]}
 
         return
 
@@ -73,3 +115,56 @@ class Policy(object):
                     best_next_action, best_next_reward = next_action, next_reward
 
         return best_next_action, best_next_reward
+
+    def eval(self, env):
+
+        observation = env.reset()
+        self.initialize(env)
+
+        """
+        The number of time steps and the number of observations should be equal after initialization
+        """
+        count = self.time_steps - 1
+        ob_iter = reversed(self.observations.keys())
+
+        """
+        TODO: extrapolate to N-step
+        """
+        while count > 0:
+
+            next_ob = self.observations[next(ob_iter)]
+            current_ob = self.observations[next(ob_iter)]
+
+            current_next_action, current_reward = self.next_action(current_ob)
+            next_next_action, next_reward = self.next_action(next_ob)
+
+            """
+                Calculate the first visit return for each (S_T, A_T) when (S_T, A_T) does not appear in the policy
+            """
+            for idx in self.observations[current_ob]:
+
+                """ Skip iteration in the event there is existing state-action. """
+                if self.observations[current_ob][idx]['next_action'] == current_next_action:
+                    continue
+
+                """
+                Calculate the first visit discounted return for the state-action and increment the number of 
+                visits. 
+                """
+                if len(self.observations[current_ob][idx]['discounted_returns']) == \
+                        self.observations[current_ob][idx]['visits']:
+                    self.observations[current_ob][idx]['discounted_returns'].append(self.gamma * 0 + next_reward)
+
+                self.observations[current_ob][idx]['discounted_returns'][-1] = \
+                    self.gamma * self.observations[current_ob][idx]['discounted_returns'] + next_reward
+
+                self.observations[current_ob][idx]['visits'] += 1
+
+            count -= 1
+
+            """
+                Set the optimal state action
+            """
+            self.set_optimal_state_action(current_ob, current_next_action)
+
+        return
--- agents/policy_iteration_agent.py
+++ agents/policy_iteration_agent.py
@@ -1,9 +1,8 @@
 import argparse
 import gym
-import sys
 from . import policy
 from gym import wrappers, logger
-from numpy.random import default_rng
+
 
 """
     The goal here is to eventually have a single policy iteration agent
@@ -21,6 +20,7 @@ class PolicyIterationAgent(object):
     policy = None  # a mapping of states to optimal actions
     time_steps = None  # time steps per episode
     gamma = None  # discount rate
+    epsilon = None
 
     def __init__(self):
         self.action_values = {}
@@ -28,114 +28,10 @@ class PolicyIterationAgent(object):
         self.policy = policy.Policy()
         self.time_steps = 20
         self.gamma = .2
+        self.epsilon = .1
 
-
-    def act(self, observation=None, reward=None, done=None):
-
-        """
-            If there is no existing policy then select an action at random.
-            If there is an existing policy select an action according to the policy
-        """
-        if len(self.policy) == 0:
-            output = self.actions_space.sample()
-        else:
-            output, _ = self.policy.next_action(observation)
-
-        return output
-
-    """
-        Initialize the policy by generating an episode following pi, 
-        an initially random epsilon soft policy.
-        The code for this is going to look odd.
-    """
-    def initialize_policy(self, env):
-
-        """ 
-            Select the epsilon greedy action according to the policy.
-            If no observation was found choose the next action at random. 
-        """
-        rng = default_rng(219215)
-        random_number = rng.random()
-
-        observation = env.reset()
-        for idx in range(self.time_steps):
-
-            """ Select an action """
-            if random_number < self.epsilon or len(self.action_values) == 0:  # Explore
-                action = env.action_space.sample()
-            elif random_number >= self.epsilon:  # Exploit if there is an exploitable option
-
-                action, _ = self.policy.next_action(current_state)
-                if action is None:
-                    action = env.action_space.sample()
-
-            """ Take the selected action """
-            observation, reward, done, _ = env.step(action)
-
-            """ Add a new state -> action mapping to the existing policy """
-            if current_state is not None:
-                self.policy.add(current_state, reward, action)
-
-            """ Store the existing state, reward and action """
-            current_state = observation
-            previous_reward = reward
-            previous_action = action
-
-        return
-
-    """
-
-    """
-    def eval(self, env):
-
-        observation = env.reset()
-        self.initialize_policy(env)
-
-        """
-        The number of time steps and the number of observations should be equal.
-        """
-        count = self.time_steps - 1
-        ob_iter = reversed(self.policy.observations.keys())
-        discounted_return = 0
-        return_count = 0
-
-        """
-        TODO extrapolate to N-step
-        """
-
-        while count > 0:
-
-            next_ob = self.policy.observations(next(ob_iter))
-            current_ob = self.policy.observations(next(ob_iter))
-
-            current_action, current_reward = self.policy.next_action(current_ob)
-            next_action, next_reward = self.policy.next_action(next_ob)
-
-            """ 
-                Calculate the first visit return
-            """
-            discounted_return = self.gamma * discounted_return + next_reward
-
-            for idx in self.observations[current_ob]:
-                self.observations[current_ob][idx]['next_action']
-
-
-            count -= 1
-
-
-
-
-            self.policy.returns[(observation_idx, action)] = self.gamma * self.policy.returns[(observation_idx, action)] + reward
-            self.policy.returns_count[(observation_idx, action)] += 1
-
-            """
-                Set the optimal state action
-            """
-        self.policy.set_optimal_state_action(observation_idx, action)
-
-
-        return
-
+    def act(self):
+        pass
 
 
 if __name__ == '__main__':
